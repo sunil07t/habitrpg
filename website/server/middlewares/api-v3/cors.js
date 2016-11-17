972a7174e04a8aa867d282a955a2866081346157
SUNILJTHAPA <sunil07t@gmail.com>
2016-11-17 12:22:02 -0800

    removed bic2cal to access control allow origin

--- website/server/middlewares/api-v3/cors.js
+++ website/server/middlewares/api-v3/cors.js
@@ -1,6 +1,6 @@
 module.exports = function corsMiddleware (req, res, next) {
   res.set({
-    'Access-Control-Allow-Origin': req.header('origin') || '*' || 'https://bic2cal.eecs.berkeley.edu',
+    'Access-Control-Allow-Origin': req.header('origin') || '*',
     'Access-Control-Allow-Methods': 'OPTIONS,GET,POST,PUT,HEAD,DELETE',
     'Access-Control-Allow-Headers': 'Content-Type,Accept,Content-Encoding,X-Requested-With,x-api-user,x-api-key',
   });
